1ee1461620e44cfe71805540965e5357c9fab456
ernesto.quispe <ernesto.cajan@gmail.com>
2024-05-10 00:33:13 -0400

    W03 learning activity

--- server.js
+++ server.js
@@ -5,11 +5,14 @@
 /* ***********************
  * Require Statements
  *************************/
+const baseController = require("./controllers/baseController")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities")
 /* ***********************
  * View Engine and Templates"
  *************************/
@@ -20,9 +23,15 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-// Index route 
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome))
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
 /* ***********************
  * Local Server Information
@@ -30,7 +39,20 @@ app.get("/", function(req, res){
  *************************/
 const port = process.env.PORT
 const host = process.env.HOST
-
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 /* ***********************
  * Log statement to confirm server operation
  *************************/
