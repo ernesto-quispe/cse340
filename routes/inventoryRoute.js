bee1b40fd8f708e547d72a2360535114595ef423
ernesto.quispe <ernesto.cajan@gmail.com>
2024-05-18 22:52:59 -0400

    Adding New Classifications and Vehicles

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,6 +2,7 @@ const express = require("express")
 const utilities = require("../utilities")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const inventoryValidate = require('../utilities/inventory-validation')
 
 // Route to build inventory by classification view
 
@@ -11,5 +12,14 @@ router.get("/detail/", utilities.handleErrors(invController.buildByInventoryId50
 router.get("/", utilities.handleErrors(invController.buildManagement));
 router.get("/add-classification", utilities.handleErrors(invController.buildAddClassification));
 router.get("/add-inventory", utilities.handleErrors(invController.buildAddInventory));
+router.post("/add-classification", 
+inventoryValidate.addClassificationRules(),
+inventoryValidate.checkClassificationData,
+utilities.handleErrors(invController.addClassification));
+router.post("/add-inventory", 
+inventoryValidate.inventoryRules(),
+inventoryValidate.checkInventoryData,
+utilities.handleErrors(invController.addVehicle));
+
 
 module.exports = router;
